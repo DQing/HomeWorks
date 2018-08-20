84deb6f6eed542561074089a4317e2977e17c09e
Qingqing Dou <qqdou@thoughtworks.com>
2018-08-20 22:34:08 +0800

    accomplish test case
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

--- src/test/java/main/ApplicationTest.java
+++ src/test/java/main/ApplicationTest.java
@@ -3,13 +3,15 @@ package main;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
     @Test
-    void should_fail() {
+    void should_return_1_to_3_Multiplication() {
         Application app = new Application();
-        assertEquals(app.test(), 2);
-        assertTrue(true);
+        String actualResult = app.Multiplication(1, 3);
+        String exceptedResult = "1 * 1 = 1\n" +
+                "1 * 2 = 2   2 * 2 = 4\n" +
+                "1 * 3 = 3   2 * 3 = 6   3 * 3 = 9\n";
+        assertEquals(exceptedResult, actualResult);
     }
 }
