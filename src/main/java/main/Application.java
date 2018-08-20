aaf803f02fd6b05016ac0728085717a291f9129c
Qingqing Dou <qqdou@thoughtworks.com>
2018-08-20 22:43:29 +0800

    add more test case
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public String Multiplication(int startNumber, int endNumber) {
         StringBuilder result = new StringBuilder();
         for (int multiplier = startNumber; multiplier <= endNumber; multiplier++) {
-            for (int anotherMultiplier = startNumber; anotherMultiplier <= multiplier; anotherMultiplier++) {
+            for (int anotherMultiplier = 1; anotherMultiplier <= multiplier; anotherMultiplier++) {
                 result.append(anotherMultiplier + " * " + multiplier + " = " + anotherMultiplier * multiplier);
                 if (anotherMultiplier < multiplier) {
                     result.append("   ");
--- src/test/java/main/ApplicationTest.java
+++ src/test/java/main/ApplicationTest.java
@@ -14,4 +14,25 @@ public class ApplicationTest {
                 "1 * 3 = 3   2 * 3 = 6   3 * 3 = 9\n";
         assertEquals(exceptedResult, actualResult);
     }
+
+    @Test
+    void should_return_2_to_3_Multiplication() {
+        Application app = new Application();
+        String actualResult = app.Multiplication(2, 3);
+        String exceptedResult = "1 * 2 = 2   2 * 2 = 4\n" +
+                "1 * 3 = 3   2 * 3 = 6   3 * 3 = 9\n";
+        assertEquals(exceptedResult, actualResult);
+    }
+
+    @Test
+    void should_return_1_to_4_Multiplication() {
+        Application app = new Application();
+        String actualResult = app.Multiplication(1, 5);
+        String exceptedResult = "1 * 1 = 1\n" +
+                "1 * 2 = 2   2 * 2 = 4\n" +
+                "1 * 3 = 3   2 * 3 = 6   3 * 3 = 9\n" +
+                "1 * 4 = 4   2 * 4 = 8   3 * 4 = 12   4 * 4 = 16\n" +
+                "1 * 5 = 5   2 * 5 = 10   3 * 5 = 15   4 * 5 = 20   5 * 5 = 25\n";
+        assertEquals(exceptedResult, actualResult);
+    }
 }
